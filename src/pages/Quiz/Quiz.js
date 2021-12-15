7e9eb5ededac5f6644b2e605ca9dca9a16219700
priKSouza <priscilakarinajump@gmail.com>
2021-12-15 20:31:47 -0300

    Finalizando quiz

--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -17,9 +17,10 @@ const Quiz = () => {
           as 18 semanas de aula na Reprograma. Foram longas semanas de
           aprendizado, choro, alegrias e muito conhecimento compartilhado.
           <br /> Esse projeto foi desenvolvido com intuito de auxiliar meninas
-          que estão começando os estudos em programação. Mas calma, as perguntas
-          são bem fáceis. O Quiz contém 3 perguntinhas sobre cada lingaguem:
-          HTML 5, CSS 3, JavaScript e React. <br />
+          que estão começando os estudos em programação a fixar comandos
+          básicos. Mas calma, as perguntas são bem fáceis. O Quiz contém 3
+          perguntinhas sobre cada lingaguem: HTML 5, CSS 3, JavaScript e React.{" "}
+          <br />
           Vamos brincar um pouco?
         </p>
       </div>
